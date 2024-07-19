6e7fd0885fadbfa842caf8cc4339d57f875211c3
mohddasif17 <159805537+mohddasif17@users.noreply.github.com>
2024-07-19 16:03:15 +0530

    Add files via upload

---
+++ charts.js
@@ -0,0 +1,700 @@
+//Semi Circle for AIR QUALITY - LIVE
+window.onload = function () {
+  var chart1 = new CanvasJS.Chart("chartContainer1", {
+    animationEnabled: true,
+    title: {
+      text: "298",
+    },
+    data: [
+      {
+        type: "doughnut",
+        startAngle: 180,
+        endAngle: -90,
+        innerRadius: "75%",
+        showInLegend: false,
+        indexLabelFontSize: 12,
+        toolTipContent: "<b>{label}:</b> {y} (#percent%)",
+        dataPoints: [
+          { y: 30, color: "#6ACF74" }, // Green section
+          { y: 40, color: "#FFC300" }, // Yellow section
+          { y: 30, color: "#FF5733" }, // Red section
+          { y: 100, color: "transparent", toolTipContent: null }, // Transparent section to complete the semi-circle
+        ],
+      },
+    ],
+  });
+  chart1.render();
+
+  //2nd doughnut
+
+  var chart1 = new CanvasJS.Chart("chartContainer2", {
+    animationEnabled: true,
+    title: {
+      text: "298",
+    },
+    data: [
+      {
+        type: "doughnut",
+        startAngle: 180,
+        endAngle: -90,
+        innerRadius: "75%",
+        showInLegend: false,
+        indexLabelFontSize: 12,
+        toolTipContent: "<b>{label}:</b> {y} (#percent%)",
+        dataPoints: [
+          { y: 30, color: "#6ACF74" }, // Green section
+          { y: 40, color: "#FFC300" }, // Yellow section
+          { y: 30, color: "#FF5733" }, // Red section
+          { y: 100, color: "transparent", toolTipContent: null }, // Transparent section to complete the semi-circle
+        ],
+      },
+    ],
+  });
+  chart1.render();
+
+  //3rd Doughnut
+
+  var chart1 = new CanvasJS.Chart("chartContainer3", {
+    animationEnabled: true,
+    title: {
+      text: "298",
+    },
+    data: [
+      {
+        type: "doughnut",
+        startAngle: 180,
+        endAngle: -90,
+        innerRadius: "75%",
+        showInLegend: false,
+        indexLabelFontSize: 12,
+        toolTipContent: "<b>{label}:</b> {y} (#percent%)",
+        dataPoints: [
+          { y: 30, color: "#6ACF74" }, // Green section
+          { y: 40, color: "#FFC300" }, // Yellow section
+          { y: 30, color: "#FF5733" }, // Red section
+          { y: 100, color: "transparent", toolTipContent: null }, // Transparent section to complete the semi-circle
+        ],
+      },
+    ],
+  });
+  chart1.render();
+
+  //4th Doughnut
+
+  var chart1 = new CanvasJS.Chart("chartContainer4", {
+    animationEnabled: true,
+    title: {
+      text: "298",
+    },
+    data: [
+      {
+        type: "doughnut",
+        startAngle: 180,
+        endAngle: -90,
+        innerRadius: "75%",
+        showInLegend: false,
+        indexLabelFontSize: 12,
+
+        toolTipContent: "<b>{label}:</b> {y} (#percent%)",
+        dataPoints: [
+          { y: 30, color: "#6ACF74" }, // Green section
+          { y: 40, color: "#FFC300" }, // Yellow section
+          { y: 30, color: "#FF5733" }, // Red section
+          { y: 100, color: "transparent", toolTipContent: null }, // Transparent section to complete the semi-circle
+        ],
+      },
+    ],
+  });
+  chart1.render();
+
+  // 5th Half Doughnut
+
+  var chart1 = new CanvasJS.Chart("chartContainer5", {
+    animationEnabled: true,
+    title: {
+      text: "298",
+    },
+    data: [
+      {
+        type: "doughnut",
+        startAngle: 180,
+        endAngle: -90,
+        innerRadius: "75%",
+        showInLegend: false,
+        indexLabelFontSize: 12,
+
+        toolTipContent: "<b>{label}:</b> {y} (#percent%)",
+        dataPoints: [
+          { y: 30, color: "#6ACF74" }, // Green section
+          { y: 40, color: "#FFC300" }, // Yellow section
+          { y: 30, color: "#FF5733" }, // Red section
+          { y: 100, color: "transparent", toolTipContent: null }, // Transparent section to complete the semi-circle
+        ],
+      },
+    ],
+  });
+  chart1.render();
+
+  //6th Half Doughnut
+
+  var chart1 = new CanvasJS.Chart("chartContainer6", {
+    animationEnabled: true,
+    title: {
+      text: "0",
+    },
+    data: [
+      {
+        type: "doughnut",
+        startAngle: 180,
+        endAngle: -90,
+        innerRadius: "75%",
+        showInLegend: false,
+        indexLabelFontSize: 12,
+
+        toolTipContent: "<b>{label}:</b> {y} (#percent%)",
+        dataPoints: [
+          { y: 30, color: "#6ACF74" }, // Green section
+          { y: 40, color: "#FFC300" }, // Yellow section
+          { y: 30, color: "#FF5733" }, // Red section
+          { y: 100, color: "transparent", toolTipContent: null }, // Transparent section to complete the semi-circle
+        ],
+      },
+    ],
+  });
+  chart1.render();
+  //7th Half Doughnut
+
+  var chart1 = new CanvasJS.Chart("chartContainer7", {
+    animationEnabled: true,
+    title: {
+      text: "0",
+    },
+    data: [
+      {
+        type: "doughnut",
+        startAngle: 180,
+        endAngle: -90,
+        innerRadius: "75%",
+        showInLegend: false,
+        indexLabelFontSize: 12,
+        toolTipContent: "<b>{label}:</b> {y} (#percent%)",
+        dataPoints: [
+          { y: 30, color: "#6ACF74" }, // Green section
+          { y: 40, color: "#FFC300" }, // Yellow section
+          { y: 30, color: "#FF5733" }, // Red section
+          { y: 100, color: "transparent", toolTipContent: null }, // Transparent section to complete the semi-circle
+        ],
+      },
+    ],
+  });
+  chart1.render();
+  //8th Half Doughnut
+
+  var chart1 = new CanvasJS.Chart("chartContainer8", {
+    animationEnabled: true,
+    title: {
+      text: "0",
+    },
+    data: [
+      {
+        type: "doughnut",
+        startAngle: 180,
+        endAngle: -90,
+        innerRadius: "75%",
+        showInLegend: false,
+        indexLabelFontSize: 12,
+        toolTipContent: "<b>{label}:</b> {y} (#percent%)",
+        dataPoints: [
+          { y: 30, color: "#6ACF74" }, // Green section
+          { y: 40, color: "#FFC300" }, // Yellow section
+          { y: 30, color: "#FF5733" }, // Red section
+          { y: 100, color: "transparent", toolTipContent: null }, // Transparent section to complete the semi-circle
+        ],
+      },
+    ],
+  });
+  chart1.render();
+  //9th Half Doughnut
+
+  var chart1 = new CanvasJS.Chart("chartContainer9", {
+    animationEnabled: true,
+    title: {
+      text: "0",
+    },
+    data: [
+      {
+        type: "doughnut",
+        startAngle: 180,
+        endAngle: -90,
+        innerRadius: "75%",
+        showInLegend: false,
+        indexLabelFontSize: 12,
+
+        toolTipContent: "<b>{label}:</b> {y} (#percent%)",
+        dataPoints: [
+          { y: 30, color: "#6ACF74" }, // Green section
+          { y: 40, color: "#FFC300" }, // Yellow section
+          { y: 30, color: "#FF5733" }, // Red section
+          { y: 100, color: "transparent", toolTipContent: null }, // Transparent section to complete the semi-circle
+        ],
+      },
+    ],
+  });
+  chart1.render();
+  //10th Half Doughnut
+
+  /* var chart1 = new CanvasJS.Chart("chartContainer19", {
+    animationEnabled: true,
+    title: {
+      text: "298",
+    },
+    data: [
+      {
+        type: "doughnut",
+        startAngle: 180,
+        endAngle: -90,
+        innerRadius: "75%",
+        showInLegend: false,
+        indexLabelFontSize: 12,
+        toolTipContent: "<b>{label}:</b> {y} (#percent%)",
+        dataPoints: [
+          { y: 30, color: "#6ACF74" }, // Green section
+          { y: 40, color: "#FFC300" }, // Yellow section
+          { y: 30, color: "#FF5733" }, // Red section
+          { y: 100, color: "transparent", toolTipContent: null }, // Transparent section to complete the semi-circle
+        ],
+      },
+    ],
+  });
+  chart1.render();*/
+
+  //Column Chart Sensor Data
+  var chart = new CanvasJS.Chart("chartContainer13", {
+    animationEnabled: true,
+    title: {
+      text: "Sensor Data",
+    },
+    axisX: {
+      interval: 1,
+      intervalType: "year",
+      valueFormatString: "YYYY",
+    },
+    axisY: {
+      suffix: "%",
+    },
+    toolTip: {
+      shared: true,
+    },
+    legend: {
+      reversed: true,
+      verticalAlign: "center",
+      horizontalAlign: "right",
+    },
+    data: [
+      {
+        type: "stackedColumn100",
+        name: "Good Quality",
+        showInLegend: true,
+        xValueFormatString: "YYYY",
+        yValueFormatString: '#,##0"%"',
+        dataPoints: [
+          { x: new Date(2010, 0), y: 40, label: "Jan-24" },
+          { x: new Date(2011, 0), y: 50, label: "Feb-24" },
+          { x: new Date(2012, 0), y: 60, label: "Mar-24" },
+          { x: new Date(2013, 0), y: 61, label: "Apr-24" },
+          { x: new Date(2014, 0), y: 63, label: "May-24" },
+          { x: new Date(2015, 0), y: 65, label: "Jun-24" },
+          { x: new Date(2016, 0), y: 67, label: "Jul-24" },
+        ],
+      },
+      {
+        type: "stackedColumn100",
+        name: "Medium Quality",
+        showInLegend: true,
+        xValueFormatString: "YYYY",
+        yValueFormatString: '#,##0"%"',
+        dataPoints: [
+          { x: new Date(2010, 0), y: 28, label: "Jan-24" },
+          { x: new Date(2011, 0), y: 18, label: "Feb-24" },
+          { x: new Date(2012, 0), y: 12, label: "Mar-24" },
+          { x: new Date(2013, 0), y: 10, label: "Apr-24" },
+          { x: new Date(2014, 0), y: 10, label: "May-24" },
+          { x: new Date(2015, 0), y: 7, label: "Jun-24" },
+          { x: new Date(2016, 0), y: 5, label: "Jul-24" },
+        ],
+      },
+      {
+        type: "stackedColumn100",
+        name: "Poor Quality",
+        showInLegend: true,
+        xValueFormatString: "YYYY",
+        yValueFormatString: '#,##0"%"',
+        dataPoints: [
+          { x: new Date(2010, 0), y: 15, label: "Jan-24" },
+          { x: new Date(2011, 0), y: 12, label: "Feb-24" },
+          { x: new Date(2012, 0), y: 10, label: "Mar-24" },
+          { x: new Date(2013, 0), y: 9, label: "Apr-24" },
+          { x: new Date(2014, 0), y: 7, label: "May-24" },
+          { x: new Date(2015, 0), y: 5, label: "Jun-24" },
+          { x: new Date(2016, 0), y: 1, label: "Jul-24" },
+        ],
+      },
+      {},
+    ],
+  });
+  chart.render();
+
+  //Chart3 configuration ODOR SENSORS – APR 2024
+  var chart3 = new CanvasJS.Chart("chartContainer12", {
+    animationEnabled: true,
+    title: {
+      text: "7:00AM-11:00PM",
+    },
+    data: [
+      {
+        type: "pie",
+        startAngle: 240,
+        yValueFormatString: '##0.00"%"',
+        indexLabel: "{label} {y}",
+        showInLegend: "true",
+        legendMarkerColor: "grey",
+        legendText: "RM4_GF_MALE",
+        dataPoints: [
+          { y: 95, label: "Good air quality" },
+          { y: 3, label: "Medium air quality" },
+          { y: 2, label: "Poor air quality" },
+        ],
+      },
+    ],
+  });
+  //RM1 - GF washroom - Male
+  chart3.render();
+  var chart4 = new CanvasJS.Chart("chartContainer10", {
+    animationEnabled: true,
+    theme: "light2",
+    title: {
+      text: "RM1 - GF washroom - Male",
+    },
+    axisX: {
+      title: "Time",
+      valueFormatString: "HH:mm",
+    },
+    axisY: {
+      title: "Odor Level",
+      interval: 100,
+      maximum: 1000,
+      stripLines: [
+        {
+          startValue: 0,
+          endValue: 300,
+          color: "#d6f5d6",
+          label: "Low",
+          labelFontColor: "#009900",
+        },
+        {
+          startValue: 300,
+          endValue: 600,
+          color: "#ffffcc",
+          label: "Medium",
+          labelFontColor: "#ffcc00",
+        },
+        {
+          startValue: 600,
+          endValue: 1000,
+          color: "#ffcccc",
+          label: "High",
+          labelFontColor: "#ff0000",
+        },
+      ],
+    },
+    data: [
+      {
+        type: "line",
+        showInLegend: true,
+        legendMarkerColor: "blue",
+        legendText: "Odor level",
+        indexLabelFontSize: 16,
+        dataPoints: [
+          { y: 200 },
+          { y: 300 },
+          { y: 400 },
+          { y: 500 },
+          { y: 600 },
+
+          {
+            y: 520,
+            y: 550,
+            indexLabel: "\u2191 highest",
+            markerColor: "red",
+            markerType: "triangle",
+          },
+          { y: 220 },
+          { y: 240 },
+          { y: 260 },
+          { y: 280 },
+          { y: 300 },
+          { y: 320 },
+          { y: 340 },
+          { y: 380 },
+          { y: 440 },
+          { y: 460 },
+          { y: 450 },
+          { y: 480 },
+          { y: 500 },
+          { y: 550 },
+
+          {
+            y: 410,
+            indexLabel: "\u2193 lowest",
+            markerColor: "DarkSlateGrey",
+            markerType: "cross",
+          },
+          { y: 500 },
+          { y: 480 },
+          { y: 510 },
+        ],
+      },
+    ],
+  });
+  chart4.render();
+  var chart5 = new CanvasJS.Chart("chartContainer14", {
+    animationEnabled: true,
+    title: {
+      text: "METER DISTRIBUTION",
+    },
+    data: [
+      {
+        type: "doughnut",
+        startAngle: 60,
+        //innerRadius: 60,
+        indexLabelFontSize: 17,
+        indexLabel: "{label} - #percent%",
+        toolTipContent: "<b>{label}:</b> {y} (#percent%)",
+        dataPoints: [
+          { y: 67, label: "Tenant meters" },
+          { y: 28, label: "Contactor meters" },
+          { y: 10, label: "AHU meters" },
+          { y: 7, label: "Others" },
+        ],
+      },
+    ],
+  });
+  chart5.render();
+  var chart6 = new CanvasJS.Chart("chartContainer15", {
+    animationEnabled: true,
+    theme: "light2", // "light1", "light2", "dark1", "dark2"
+    title: {
+      text: "METER COUNT",
+    },
+    axisY: {
+      title: "Reserves(MMbbl)",
+    },
+    data: [
+      {
+        type: "column",
+        showInLegend: true,
+        legendMarkerColor: "grey",
+        legendText: "MMbbl = one million barrels",
+        dataPoints: [
+          { y: 300878, label: "Tenant meters=272" },
+          { y: 266455, label: "Contactor meters=39" },
+          { y: 169709, label: "AHU meters=23" },
+          { y: 158400, label: "Others=22" },
+        ],
+      },
+    ],
+  });
+  chart6.render();
+  //Pie Chart for EB Consumption Apr-2024
+  var chart7 = new CanvasJS.Chart("chartContainer16", {
+    theme: "light2",
+    animationEnabled: true,
+    title: {
+      text: "EB CONSUMPTION DISTRIBUTION",
+    },
+    subtitles: [
+      {
+        text: "Delhi India, 2024",
+        fontSize: 16,
+      },
+    ],
+    data: [
+      {
+        type: "pie",
+        indexLabelFontSize: 18,
+        radius: 80,
+        indexLabel: "{label} - {y}",
+        yValueFormatString: '###0.0"%"',
+        click: explodePie,
+        dataPoints: [
+          { y: 42, label: "Tenant" },
+          { y: 21, label: "Contactor" },
+          { y: 24.5, label: "AHU " },
+          { y: 9, label: "Others" },
+        ],
+      },
+    ],
+  });
+  chart7.render();
+
+  function explodePie(e) {
+    for (var i = 0; i < e.dataSeries.dataPoints.length; i++) {
+      if (i !== e.dataPointIndex) e.dataSeries.dataPoints[i].exploded = false;
+    }
+  }
+  //EB CONSUMPTION KWH ( MW)
+  var chart8 = new CanvasJS.Chart("chartContainer17", {
+    animationEnabled: true,
+    theme: "light2", // "light1", "light2", "dark1", "dark2"
+    title: {
+      text: "EB CONSUMPTION KWH ( MW)",
+    },
+    axisY: {
+      title: "Reserves(MMbbl)",
+    },
+    data: [
+      {
+        type: "column",
+        showInLegend: true,
+        legendMarkerColor: "grey",
+        legendText: "MMbbl = one million barrels",
+        dataPoints: [
+          { y: 300878, label: "Tenant, 8.2" },
+          { y: 266455, label: "Contactor, 3.2" },
+          { y: 169709, label: "AHU, 1.4" },
+          { y: 158400, label: "Others, 1.2" },
+        ],
+      },
+    ],
+  });
+  chart8.render();
+  //EB Consumption- 2024
+
+  var chart9 = new CanvasJS.Chart("chartContainer18", {
+    animationEnabled: true,
+    theme: "light2",
+    title: {
+      text: "Total EB KWH (MW)",
+    },
+    axisX: {
+      valueFormatString: "DD MMM",
+      crosshair: {
+        enabled: true,
+        snapToDataPoint: true,
+      },
+    },
+    axisY: {
+      title: "KWH",
+      includeZero: true,
+      crosshair: {
+        enabled: true,
+      },
+    },
+    toolTip: {
+      shared: true,
+    },
+    legend: {
+      cursor: "pointer",
+      verticalAlign: "bottom",
+      horizontalAlign: "left",
+      dockInsidePlotArea: true,
+      itemclick: toogleDataSeries,
+    },
+    data: [
+      {
+        type: "line",
+        showInLegend: true,
+        name: "EB",
+        markerType: "square",
+        xValueFormatString: "DD MMM, YYYY",
+        color: "#F08080",
+        dataPoints: [
+          { x: new Date(2017, 0, 3), y: 650 },
+          { x: new Date(2017, 0, 4), y: 700 },
+          { x: new Date(2017, 0, 5), y: 710 },
+          { x: new Date(2017, 0, 6), y: 658 },
+          { x: new Date(2017, 0, 7), y: 734 },
+          { x: new Date(2017, 0, 8), y: 963 },
+          { x: new Date(2017, 0, 9), y: 847 },
+          { x: new Date(2017, 0, 10), y: 853 },
+          { x: new Date(2017, 0, 11), y: 869 },
+          { x: new Date(2017, 0, 12), y: 943 },
+          { x: new Date(2017, 0, 13), y: 970 },
+          { x: new Date(2017, 0, 14), y: 869 },
+          { x: new Date(2017, 0, 15), y: 890 },
+          { x: new Date(2017, 0, 16), y: 930 },
+        ],
+      },
+      {
+        type: "line",
+        showInLegend: true,
+        name: "DG",
+        lineDashType: "dash",
+        dataPoints: [
+          { x: new Date(2017, 0, 3), y: 510 },
+          { x: new Date(2017, 0, 4), y: 560 },
+          { x: new Date(2017, 0, 5), y: 540 },
+          { x: new Date(2017, 0, 6), y: 558 },
+          { x: new Date(2017, 0, 7), y: 544 },
+          { x: new Date(2017, 0, 8), y: 693 },
+          { x: new Date(2017, 0, 9), y: 657 },
+          { x: new Date(2017, 0, 10), y: 663 },
+          { x: new Date(2017, 0, 11), y: 639 },
+          { x: new Date(2017, 0, 12), y: 673 },
+          { x: new Date(2017, 0, 13), y: 660 },
+          { x: new Date(2017, 0, 14), y: 562 },
+          { x: new Date(2017, 0, 15), y: 643 },
+          { x: new Date(2017, 0, 16), y: 570 },
+        ],
+      },
+    ],
+  });
+  chart9.render();
+
+  function toogleDataSeries(e) {
+    if (typeof e.dataSeries.visible === "undefined" || e.dataSeries.visible) {
+      e.dataSeries.visible = false;
+    } else {
+      e.dataSeries.visible = true;
+    }
+    chart9.render();
+  }
+  //
+  var chart10 = new CanvasJS.Chart("chartContainer19", {
+    animationEnabled: true,
+    title: {
+      text: "Total EB KWH (MW)",
+    },
+    axisX: {
+      interval: 1,
+    },
+    axisY2: {
+      interlacedColor: "rgba(1,77,101,.2)",
+      gridColor: "rgba(1,77,101,.1)",
+      title: "Number of Companies",
+    },
+    data: [
+      {
+        type: "bar",
+        name: "companies",
+        color: "#01650b",
+        axisYType: "secondary",
+        dataPoints: [
+          { y: 3, label: "Jan-24" },
+          { y: 7, label: "Feb-24" },
+          { y: 5, label: "Mar-24" },
+          { y: 9, label: "Apr-24" },
+          { y: 7, label: "May-24" },
+          { y: 7, label: "Jun-24" },
+          { y: 9, label: "Jul-24" },
+          { y: 8, label: "Aug-24" },
+          { y: 11, label: "Sep-24" },
+          { y: 15, label: "Oct-24" },
+          { y: 12, label: "Nov-24" },
+          { y: 15, label: "Dec-24" },
+        ],
+      },
+    ],
+  });
+  chart10.render();
+};
